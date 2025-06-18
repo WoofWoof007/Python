b24078276714815c06f37845cf9f8fa7ffa2acef
WoofWoof007 <mayuckbiswas2006@gmail.com>
2025-06-19 01:04:56 +0530

    Input

--- varibles.py
+++ varibles.py
@@ -2,5 +2,15 @@ Name = "Mayuck Biswas"
 Age = 19
 Roll = 123241303075
 
-print(Name)
-print(Age)
+a = 2
+b = 3
+sum = a + b
+sub = a - b
+print(sub)
+print(sum)
+
+A,B="2",3
+txt = "@"
+print((A+txt)*B)
+
+#Comment :)
